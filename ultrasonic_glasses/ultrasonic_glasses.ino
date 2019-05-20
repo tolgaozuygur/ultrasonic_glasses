54441f4f2a626b601ca3075e8710763740faea7e
tolgaozuygur <tolgaozuygur@gmail.com>
2019-05-21 01:57:51 +0300

    Add files via upload

---
+++ ultrasonic_glasses/ultrasonic_glasses.ino
@@ -0,0 +1,44 @@
+/*
+ Ultrasonic Glasses Project for Visually Impaired
+ by Tolga Ozuygur
+*/
+
+const int anPin = 0;
+long mm;
+int buzzerPin = 3;
+long buzzerBeepDuration = 40;
+long buzzerBeepMillis = 0;
+long buzzerFrequencyDuration = 0;
+long buzzerPreviousBeepMillis = 0;
+float mmToBuzzerFrequencyMultiplier = 0.6;
+
+void setup(){
+  Serial.begin(9600);
+  pinMode(buzzerPin, OUTPUT);
+  calibrationWarning(3000, 500, 100);
+}
+
+void loop(){
+  mm = analogRead(anPin) * 5;
+  buzzerFrequencyDuration = mm * mmToBuzzerFrequencyMultiplier;
+  if(buzzerPreviousBeepMillis + buzzerFrequencyDuration < millis()){
+    buzzerPreviousBeepMillis =  millis();
+    buzzerBeepMillis = millis() + buzzerBeepDuration;
+    digitalWrite(buzzerPin, HIGH);
+  }
+  if(buzzerBeepMillis < millis()){
+    digitalWrite(buzzerPin, LOW);
+  }
+  Serial.println(buzzerFrequencyDuration);
+  delay(5);
+}
+
+void calibrationWarning(int totalDur, int beepDur, int silenceDur){
+  int oneBeep = beepDur + silenceDur;
+  for(int i = 0; i < totalDur; i+=oneBeep) {
+    digitalWrite(buzzerPin, HIGH);
+    delay(beepDur);
+    digitalWrite(buzzerPin, LOW);
+    delay(silenceDur);
+  }
+}
